bf8fac36b1bda2af08b5e2a6fad74cbd426fe3d5
sohil-khann <khaa40408@gmail.com>
2024-11-05 10:40:30 +0530

    js

--- javascript/index01.js
+++ javascript/index01.js
@@ -1,15 +1,22 @@
+const arr = [3, "4", 5, "6", 7, "8", 9, 10, 11, "12"];
+let r = Math.round(Math.random() * (12 - 3) + 3);
 
-    var input = prompt("Enter something:");
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i]==r){
+   console.log(typeof(arr[i])+"  : "+arr[i]);
+break;}
+}
+console.log(5*"8"-4);
 
-    if (isNaN(input)) {
-        console.log("The input is a string.");
-    } else {
-        var number = parseFloat(input);
-        if (number > 0) {
-            console.log("The input is a positive number.");
-        } else if (number < 0) {
-            console.log("The input is a negative number.");
-        } else {
-            console.log("The input is zero.");
-        }
-    }
+//function/////////////////////////////////////
+function sum(x,y){
+    console.log(x+y);
+}
+sum(5,6)
+//arrow function 
+const arrow=(c,v)=>{
+console.log(c+v);
+
+}
+arrow(22,3)
+
